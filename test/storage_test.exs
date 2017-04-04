9190b29498d7614d25211b08c4eea2ac00726d76
Winand, Christopher <cwinand@meetclutch.com>
2017-04-04 13:34:19 -0400

    Add async tag to current tests

--- test/storage_test.exs
+++ test/storage_test.exs
@@ -1,5 +1,5 @@
 defmodule Bookmarksync.StorageTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   import Bookmarksync.Storage
 
--- test/urlbuilder_test.exs
+++ test/urlbuilder_test.exs
@@ -1,5 +1,5 @@
 defmodule Bookmarksync.URLBuilderTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   import Bookmarksync.URLBuilder
 
