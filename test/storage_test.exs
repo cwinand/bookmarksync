1d6cc0d2a8dab0f87c128f16f044144f1097ddcc
Winand, Christopher <cwinand@meetclutch.com>
2017-04-04 13:35:20 -0400

    Update storage tests for refactored Storage module

--- test/storage_test.exs
+++ test/storage_test.exs
@@ -3,25 +3,72 @@ defmodule Bookmarksync.StorageTest do
 
   import Bookmarksync.Storage
 
+  @config Application.get_env( :bookmarksync, :key_file )
+
   setup do
-    File.write!( "data/data_test.json", "{}" )
+    # Testing File IO is hard. We need to wipe and recreate the files each test so we aren't
+    # having race conditions when a test expects a file to exist when it doesn't, or vice versa.
+
+    cache = "data/test"
+
+    File.write!( @config, "{}" )
+    File.mkdir!( cache )
+
+    on_exit fn ->
+      File.rm_rf!( @config )
+      File.rm_rf!( cache )
+    end
   end
 
-  test "opens the storage file as a map" do
-    assert is_map open()
+  test "opens a json file as a map" do
+    assert is_map open( @config )
   end
 
   test "saves a map as encoded JSON" do
-    assert save %{ "key" => "value" } == :ok
+    assert save( %{}, @config ) == :ok
   end
 
-  test "sets a value on a non-nested key" do
-    assert set( "key", "value" ) == :ok
+  test "gets and sets key-value pairs in the config file" do
+    assert set_config( %{}, [ "key" ] ) == :ok
+    assert is_map get_config( [ "key" ] )
+
+    assert set_config( "value", [ "key", "nested_key" ] ) == :ok
+    assert get_config( [ "key", "nested_key" ] ) == "value"
+
+    assert set( "value", :config, [ "key2" ] ) == :ok
+    assert get( :config, [ "key2" ] ) == "value"
   end
 
-  test "sets a value on a nested key" do
-    set( "key", %{} )
-    assert set( [ "key", "child_key" ], "value" ) == :ok
+  test "sets a JSON cache file named with a timestamp" do
+    data = %{ "key" => "value" }
+    assert set_cache( data, "test", "12345" ) == data
+    assert set( data, :cache, [ "test", 12345 ] ) == data
   end
 
+  test "opens a cache file as a map" do
+    File.write( "data/test/12345.json", "{}" )
+    assert is_map get_cache( "test", "12345" )
+    assert is_map get( :cache, [ "test", 12345 ] )
+  end
+
+  test "gets the timestamp of the most recent cache file" do
+    File.write( "data/test/12345.json", "{}" )
+    assert latest_cache( "test" ) == 12345
+  end
+
+  test "check if the current cache is out of date" do
+    File.write( "data/test/12345.json", "{}" )
+    assert stale_cache?( "test", 12346 )
+    refute stale_cache?( "test", 12344 )
+  end
+
+  test "clears the current cache" do
+    file ="data/test/12345.json"
+
+    File.write( file, "{}" )
+    assert File.exists?( file )
+
+    flush_cache( "test" )
+    refute File.exists?( file )
+  end
 end
